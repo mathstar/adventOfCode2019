b0b6ffb664a9c786dec4ad8e0768f58eff95afb4
Mathias Staricka <staricm@amazon.com>
2019-12-04 22:04:41 -0800

    day 5

---
+++ src/com/staricka/aoc2019/days/Day5.java
@@ -0,0 +1,39 @@
+package com.staricka.aoc2019.days;
+
+import com.staricka.aoc2019.intcode.IntCodeProgram;
+import com.staricka.aoc2019.util.AocDay;
+import com.staricka.aoc2019.util.AocInputStream;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+
+public class Day5 extends AocDay {
+    @Override
+    public void part1() throws Exception {
+        try (AocInputStream inputStream = new AocInputStream("day4a.txt")){
+            String program = inputStream.getAll();
+            String input = "1\n";
+            IntCodeProgram intCodeProgram = new IntCodeProgram(program, new StringReader(input), System.out);
+            intCodeProgram.run();
+        }
+    }
+
+    @Override
+    public void part2() throws Exception {
+        try (AocInputStream inputStream = new AocInputStream("day4a.txt")){
+            String program = inputStream.getAll();
+            String input = "5\n";
+            IntCodeProgram intCodeProgram = new IntCodeProgram(program, new StringReader(input), System.out);
+            intCodeProgram.run();
+        }
+    }
+
+    @Override
+    public void test() throws Exception {
+        BufferedReader inputReader = new BufferedReader(new InputStreamReader(System.in));
+        String program = inputReader.readLine();
+        IntCodeProgram intCodeProgram = new IntCodeProgram(program, System.in, System.out);
+        intCodeProgram.run();
+    }
+}
--- src/com/staricka/aoc2019/intcode/IntCodeProgram.java
+++ src/com/staricka/aoc2019/intcode/IntCodeProgram.java
@@ -2,6 +2,13 @@ package com.staricka.aoc2019.intcode;
 
 import com.staricka.aoc2019.util.AocDay;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -12,12 +19,28 @@ public class IntCodeProgram {
     private List<Integer> memory;
     private int programCounter;
     private boolean running;
+    private BufferedReader inputReader;
+    private BufferedWriter outputWriter;
 
-    public IntCodeProgram(final String input) {
-        memory = Arrays.stream(input.split(",")).map(Integer::parseInt).collect(Collectors.toList());
+    public IntCodeProgram(final String program) throws Exception {
+        memory = Arrays.stream(program.split(",")).map(Integer::parseInt).collect(Collectors.toList());
     }
 
-    public void run() {
+    public IntCodeProgram(final String program, final InputStream inputStream, final OutputStream outputStream)
+            throws Exception {
+        memory = Arrays.stream(program.split(",")).map(Integer::parseInt).collect(Collectors.toList());
+        inputReader = new BufferedReader(new InputStreamReader(inputStream));
+        outputWriter = new BufferedWriter(new OutputStreamWriter(outputStream));
+    }
+
+    public IntCodeProgram(final String program, final Reader inputReader, final OutputStream outputStream)
+            throws Exception {
+        memory = Arrays.stream(program.split(",")).map(Integer::parseInt).collect(Collectors.toList());
+        this.inputReader = new BufferedReader(inputReader);
+        outputWriter = new BufferedWriter(new OutputStreamWriter(outputStream));
+    }
+
+    public void run() throws Exception {
         programCounter = 0;
         running = true;
         AocDay.logDebug(memoryString());
@@ -25,27 +48,89 @@ public class IntCodeProgram {
             step();
             AocDay.logDebug(memoryString());
         }
+        if (inputReader != null) {
+            inputReader.close();
+        }
+        if (outputWriter != null) {
+            outputWriter.close();
+        }
     }
 
-    private void step() {
-        OpCode opCode = OpCode.lookupOpCode(getValue(programCounter));
+    private void step() throws Exception {
+        int numericOpCode = getValue(programCounter);
+        AocDay.logDebug("OpCode: %d, PC: %d", numericOpCode, programCounter);
+        OpCode opCode = OpCode.lookupOpCode(numericOpCode);
+        boolean incrementProgramCounter = true;
         switch (opCode) {
             case ADD: {
                 setValue(getValue(programCounter + 3),
-                        getValue(getValue(programCounter + 1)) + getValue(getValue(programCounter + 2)));
+                        getValue(programCounter + 1, ParameterMode.lookupParameterCode(numericOpCode, 0)) + getValue(
+                                programCounter + 2, ParameterMode.lookupParameterCode(numericOpCode, 1)));
                 break;
             }
             case MULTIPLY: {
                 setValue(getValue(programCounter + 3),
-                        getValue(getValue(programCounter + 1)) * getValue(getValue(programCounter + 2)));
+                        getValue(programCounter + 1, ParameterMode.lookupParameterCode(numericOpCode, 0)) * getValue(
+                                programCounter + 2, ParameterMode.lookupParameterCode(numericOpCode, 1)));
+                break;
+            }
+            case STORE: {
+                setValue(getValue(programCounter + 1), readInput());
+                break;
+            }
+            case RETRIEVE: {
+                writeOutput(getValue(programCounter + 1, ParameterMode.lookupParameterCode(numericOpCode, 0)));
+                break;
+            }
+            case JIT: {
+                if (getValue(programCounter + 1, ParameterMode.lookupParameterCode(numericOpCode, 0)) != 0) {
+                    programCounter = getValue(programCounter + 2, ParameterMode.lookupParameterCode(numericOpCode, 1));
+                    incrementProgramCounter = false;
+                }
+                break;
+            }
+            case JIF: {
+                if (getValue(programCounter + 1, ParameterMode.lookupParameterCode(numericOpCode, 0)) == 0) {
+                    programCounter = getValue(programCounter + 2, ParameterMode.lookupParameterCode(numericOpCode, 1));
+                    incrementProgramCounter = false;
+                }
+                break;
+            }
+            case LT: {
+                if (getValue(programCounter + 1, ParameterMode.lookupParameterCode(numericOpCode, 0)) < getValue(
+                        programCounter + 2, ParameterMode.lookupParameterCode(numericOpCode, 1))) {
+                    setValue(getValue(programCounter + 3), 1);
+                } else {
+                    setValue(getValue(programCounter + 3), 0);
+                }
+                break;
+            }
+            case EQ: {
+                if (getValue(programCounter + 1, ParameterMode.lookupParameterCode(numericOpCode, 0)) == getValue(
+                        programCounter + 2, ParameterMode.lookupParameterCode(numericOpCode, 1))) {
+                    setValue(getValue(programCounter + 3), 1);
+                } else {
+                    setValue(getValue(programCounter + 3), 0);
+                }
                 break;
             }
             case STOP: {
                 running = false;
                 break;
             }
+        } if (incrementProgramCounter) {
+            programCounter += opCode.parameterCount + 1;
         }
-        programCounter += opCode.parameterCount + 1;
+    }
+
+    private int getValue(final int address, final ParameterMode parameterMode) {
+        switch (parameterMode) {
+            case POSITION:
+                return getValue(getValue(address));
+            case IMMEDIATE:
+                return getValue(address);
+        }
+        return -1;
     }
 
     public int getValue(final int address) {
@@ -54,6 +139,14 @@ public class IntCodeProgram {
 
     public void setValue(final int address, final int value) {
         memory.set(address, value);
+    }
+
+    private int readInput() throws Exception {
+        return Integer.parseInt(inputReader.readLine());
+    }
+
+    private void writeOutput(final int output) throws Exception {
+        outputWriter.write(output + "\n");
     }
 
     public void printMemory() {
@@ -65,7 +158,7 @@ public class IntCodeProgram {
     }
 
     private enum OpCode {
-        ADD(1, 3), MULTIPLY(2, 3), STOP(99, 0);
+        ADD(1, 3), MULTIPLY(2, 3), STORE(3, 1), RETRIEVE(4, 1), JIT(5, 2), JIF(6, 2), LT(7, 3), EQ(8, 3), STOP(99, 0);
         private int opCode;
         private int parameterCount;
 
@@ -78,7 +171,30 @@ public class IntCodeProgram {
         }
 
         private static OpCode lookupOpCode(final int opCode) {
-            return lookupMap.get(opCode);
+            return lookupMap.get(opCode % 100);
+        }
+    }
+
+    private enum ParameterMode {
+        POSITION('0'), IMMEDIATE('1');
+
+        private static Map<Character, ParameterMode> lookupMap =
+                Arrays.stream(ParameterMode.values()).collect(Collectors.toMap(p -> p.code, Function.identity()));
+
+        private char code;
+
+        ParameterMode(char code) {
+            this.code = code;
+        }
+
+        private static ParameterMode lookupParameterCode(final int opCode, final int parameterIndex) {
+            char[] digits = String.valueOf(opCode).toCharArray();
+            int index = digits.length - 3 - parameterIndex;
+            //AocDay.logDebug("oc: %d, pi: %d, i: %d, dig: %s", opCode, parameterIndex, index, Arrays.toString(digits));
+            if (index < 0) {
+                return POSITION;
+            }
+            return lookupMap.get(digits[index]);
         }
     }
 
