e22b4850c8d2053cf8518f25e5170ac8473da1af
Mathias Staricka <staricm@amazon.com>
2019-12-23 19:15:48 -0800

    day 14 part 2

--- src/main/java/com/staricka/aoc2019/days/Day14.java
+++ src/main/java/com/staricka/aoc2019/days/Day14.java
@@ -4,6 +4,7 @@ import com.staricka.aoc2019.util.AocDay;
 import com.staricka.aoc2019.util.AocInputStream;
 
 import java.util.*;
+import java.util.Map.Entry;
 import java.util.stream.Collectors;
 
 public class Day14 extends AocDay {
@@ -18,133 +19,92 @@ public class Day14 extends AocDay {
 
     @Override
     public void part2() throws Exception {
-        try (final AocInputStream inputStream = new AocInputStream("day14s4.txt")) {
+        try (final AocInputStream inputStream = new AocInputStream("day14.txt")) {
             List<ReactionRule> reactionRules = inputStream.lines().map(ReactionRule::new).collect(Collectors.toList());
-            Map<String, List<ReactionRule>> reactionRulesByOutput = reactionRules.stream().collect(Collectors.groupingBy(r -> r.getOutput().getSymbol()));
+            Map<String, List<ReactionRule>> reactionRulesByOutput =
+                    reactionRules.stream().collect(Collectors.groupingBy(r -> r.getOutput().getSymbol()));
             long ore = 1000000000000L;
-            logInfo("Possible fuel: %d", findMaxFuel(reactionRulesByOutput, ore));
+            logInfo("Possible fuel: %d", binaryMaxFuel(reactionRulesByOutput, ore));
         }
     }
 
     private long findMinOre(final Map<String, List<ReactionRule>> reactionRulesByOutput) {
-        StateController controller = new StateController("ORE", new ReactionComponent("FUEL", 1), reactionRulesByOutput);
+        StateController controller =
+                new StateController("ORE", new ReactionComponent("FUEL", 1), reactionRulesByOutput);
 
-        return controller.best(Comparator.<State, Long>comparing(s -> s.chemicalsNeeded.getNeeded("ORE")).reversed()).chemicalsNeeded.getNeeded("ORE");
+        return controller.best().chemicalsNeeded.getNeeded("ORE");
     }
 
-    private long findMaxFuel(final Map<String, List<ReactionRule>> reactionRulesByOutput, final long ore) {
-        StateController controller = new StateController("ORE", new ReactionComponent("FUEL", 1), reactionRulesByOutput);
-        State ideal = controller.best(Comparator.<State, Long>comparing(s -> s.chemicalsNeeded.getNeeded("ORE")).reversed());
-        logInfo(ideal.rulesApplied);
+    private long binaryMaxFuel(final Map<String, List<ReactionRule>> reactionRulesByOutput, final long ore) {
+        StateController controller =
+                new StateController("ORE", new ReactionComponent("FUEL", 1), reactionRulesByOutput);
+        State ideal = controller.best();
 
-        long fuelProduced = 0;
-        while (ideal.chemicalsNeeded.getNeeded("ORE") < ore) {
-            fuelProduced++;
-            //ideal.replayRules();
-            controller = new StateController(ideal, "FUEL", reactionRulesByOutput);
-            ideal = controller
-                    .best(Comparator.<State, Long>comparing(s -> s.chemicalsNeeded.getNeeded("ORE")).reversed());
-            //logInfo(ideal.chemicalsNeeded.chemicalsNeeded);
-            if (ideal.chemicalsNeeded.chemicalsNeeded.values().stream().allMatch(x -> x >= 0)) {
-                logInfo("cycle");
+        long lower = ore / ideal.chemicalsNeeded.getNeeded("ORE");
+        long upper = lower * 10;
+        while (lower < upper) {
+            long current = (upper - lower) / 2 + lower;
+            controller = new StateController("ORE", new ReactionComponent("FUEL", current), reactionRulesByOutput);
+            ideal = controller.best();
+
+            if (ideal.chemicalsNeeded.getNeeded("ORE") > ore) {
+                upper = current;
+            } else if (current == lower) {
+                return lower;
+            } else {
+                lower = current;
             }
         }
-        return fuelProduced;
+        return lower;
     }
 
+    /**
+     * This is a remnant of when I assumed that there could be more than one rule with the same output. It has since
+     * been stripped down for optimization when I discovered that there were no such repeats.
+     */
     private static class StateController {
-        List<State> states = new ArrayList<>();
+        private final State state;
 
         private StateController(final String target, final ReactionComponent initial,
                 final Map<String, List<ReactionRule>> reactionRulesByOutput) {
             ChemicalsNeeded chemicalsNeeded = new ChemicalsNeeded(target);
             chemicalsNeeded.add(initial.getSymbol(), initial.getQuantity());
-            State state = new State(chemicalsNeeded, reactionRulesByOutput, this);
-
-            states.add(state);
+            state = new State(chemicalsNeeded, reactionRulesByOutput, this);
             state.run();
         }
 
-        private StateController(final State previous, final String target,
-                final Map<String, List<ReactionRule>> reactionRulesByOutput) {
-            ChemicalsNeeded chemicalsNeeded = previous.chemicalsNeeded;
-            chemicalsNeeded.add(target, 1);
-            State state = new State(chemicalsNeeded, reactionRulesByOutput, this);
-
-            states.add(state);
-            state.run();
-        }
-
-        private void branch(final State state) {
-            states.add(state);
-            state.run();
-        }
-
-        private State best(final Comparator<State> comparator) {
-            return states.stream().max(comparator).get();
+        private State best() {
+            return state;
         }
     }
 
     private static class State implements Runnable{
         ChemicalsNeeded chemicalsNeeded;
         Map<String, List<ReactionRule>> reactionRulesByOutput;
-        ReactionComponent bootstrapNeeded;
-        ReactionRule bootstrapRule;
         StateController controller;
-        List<ReactionRule> rulesApplied;
 
         public State(ChemicalsNeeded chemicalsNeeded, Map<String, List<ReactionRule>> reactionRulesByOutput, StateController controller) {
             this.chemicalsNeeded = chemicalsNeeded;
             this.reactionRulesByOutput = reactionRulesByOutput;
             this.controller = controller;
-            rulesApplied = new ArrayList<>();
-        }
-
-        public State(ChemicalsNeeded chemicalsNeeded, Map<String, List<ReactionRule>> reactionRulesByOutput, List<ReactionRule> rulesApplied, ReactionComponent bootstrapNeeded, ReactionRule bootstrapRule, StateController controller) {
-            this.chemicalsNeeded = chemicalsNeeded;
-            this.reactionRulesByOutput = reactionRulesByOutput;
-            this.bootstrapNeeded = bootstrapNeeded;
-            this.bootstrapRule = bootstrapRule;
-            this.controller = controller;
         }
 
         @Override
         public void run() {
-            if(bootstrapNeeded != null && bootstrapRule != null) {
-                applyRule(bootstrapRule);
-                logRule(bootstrapRule);
-            }
-
             while (!chemicalsNeeded.done()) {
                 ReactionComponent needed = chemicalsNeeded.nextNeeded();
                 List<ReactionRule> reactionRules = reactionRulesByOutput.get(needed.getSymbol());
-                if (reactionRules.size() == 1) {
-                    ReactionRule rule = reactionRules.get(0);
-                    applyRule(rule);
-                    logRule(rule);
-                } else {
-                    List<ReactionRule> extraRules = reactionRules.subList(1, reactionRules.size());
-                    extraRules.stream().map(r -> new State(chemicalsNeeded.copy(), reactionRulesByOutput, rulesApplied, needed, r, controller)).forEach(controller::branch);
-                    ReactionRule rule = reactionRules.get(0);
-                    applyRule(rule);
-                    logRule(rule);
-                }
+                ReactionRule rule = reactionRules.get(0);
+                applyRule(rule, needed.quantity);
             }
         }
 
-        private void applyRule(final ReactionRule rule) {
-            chemicalsNeeded.subtract(rule.getOutput().getSymbol(), rule.getOutput().getQuantity());
-            for(ReactionComponent input : rule.getInputs()) {
-                chemicalsNeeded.add(input.getSymbol(), input.getQuantity());
+        private void applyRule(final ReactionRule rule, final long quantityNeeded) {
+            long multiple = (long) Math.ceil((double) quantityNeeded / rule.getOutput().quantity);
+            chemicalsNeeded.subtract(rule.getOutput().getSymbol(), rule.getOutput().getQuantity() * multiple);
+            for (ReactionComponent input : rule.getInputs()) {
+                chemicalsNeeded.add(input.getSymbol(), input.getQuantity() * multiple);
             }
-        }
-
-        private void logRule(final ReactionRule rule) {
-            rulesApplied.add(rule);
-        }
-
-        private void replayRules() {
-            rulesApplied.forEach(this::applyRule);
         }
     }
 
@@ -154,12 +114,6 @@ public class Day14 extends AocDay {
 
         private ChemicalsNeeded(final String target) {
             this.target = target;
-        }
-
-        private ChemicalsNeeded copy() {
-            ChemicalsNeeded copy = new ChemicalsNeeded(target);
-            copy.chemicalsNeeded = new HashMap<>(chemicalsNeeded);
-            return copy;
         }
 
         private long getNeeded(final String symbol) {
@@ -188,7 +142,12 @@ public class Day14 extends AocDay {
         }
 
         private boolean done() {
-            return chemicalsNeeded.entrySet().stream().noneMatch(e -> !e.getKey().equals(target) && e.getValue() > 0);
+            for (Entry<String, Long> e : chemicalsNeeded.entrySet()) {
+                if (!e.getKey().equals(target) && e.getValue() > 0) {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 
